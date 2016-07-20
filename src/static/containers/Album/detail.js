3e3640eb13e8c0a37fe8507ea71f5c2265d112ee
joy <joy.jing1@gmail.com>
2016-07-20 11:06:13 -0700

    Display photos using Masonry

--- src/static/containers/Album/detail.js
+++ src/static/containers/Album/detail.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router';
+// import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 // import logoImage from './images/react-logo.png';
 import * as actionCreators from '../../actions/photo';
+
+const Masonry = require('react-masonry-component');
+const masonryOptions = {
+    transitionDuration: 0.1
+};
 
 class AlbumDetailView extends React.Component {
 
@@ -15,34 +20,50 @@ class AlbumDetailView extends React.Component {
 
     componentWillMount() {
         // console.log("containers/AlbumDetail/index.js componentWillMount - about to dataFetchPhotos");
-        const token = this.props.token;
-        this.props.actions.dataFetchPhotos(token, this.props.params.id);
+        // const token = this.props.token;
+        this.props.actions.dataFetchPhotos(this.props.token, this.props.params.id);
     }
 
 
     render() {
+      const childElements = this.props.photos.map( photo => {
+          return (
+              <li className="image-element-class" key={photo.id}>
+                  <img src={photo.image_url} />
+              </li>
+           );
+      });
+
         return (
             <div className="container">
                 <h2>{this.props.curr_album.title}</h2>
                 <h3>{this.props.curr_album.description}</h3>
-                <ul>
-                  {this.props.photos.map( photo => {
-                    return (
-                      <li key={photo.id}>
-                        <a>
-                          <img src={photo.image_url}></img>
-                          <h3>{photo.caption}</h3>
-                          </a>
-                      </li>
-                    );
-                  })}
-                </ul>
+
+                <Masonry
+                  className={'my-gallery-class'}
+                  elementType={'ul'}
+                  options={masonryOptions}
+                  disableImagesLoaded={false}
+                  updateOnEachImageLoad={false}
+                  >
+                  {childElements}
+                </Masonry>
             </div>
         );
     }
 }
 
 
+// {this.props.photos.map( photo => {
+//   return (
+//     <li key={photo.id}>
+//       <a>
+//         <img src={photo.image_url}></img>
+//         <h3>{photo.caption}</h3>
+//       </a>
+//     </li>
+//   );
+// })}
 
 const mapStateToProps = (state) => {
   let curr_album = state.photos.curr_album ? state.photos.curr_album[0] : {}
