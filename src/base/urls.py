50d1042ded30b804c1646c312d0353595c578a45
corleyma <mattcorley@gmail.com>
2016-07-24 02:31:56 -0700

    changes import path in base.urls to be src.base.MODULE (based on Sentry error logs)

--- src/base/urls.py
+++ src/base/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from base import views as base_views
+from src.base import views as base_views
 
 urlpatterns = [
     url(r'', base_views.ProtectedDataView.as_view(), name='protected_data'),
