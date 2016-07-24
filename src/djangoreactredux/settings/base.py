b085c335dbc5e3165dc7fb2d6324f543539b872c
joy <joy.jing1@gmail.com>
2016-07-23 21:18:06 -0700

    Change static back to static_dist

--- src/djangoreactredux/settings/base.py
+++ src/djangoreactredux/settings/base.py
@@ -60,7 +60,7 @@ ACCOUNT_ACTIVATION_DAYS = 7  # days
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static_dist'),
 )
 
 # store static files locally and serve with whitenoise
