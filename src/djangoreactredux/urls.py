07a7520d934963b5ce4770870628bdf6831c1818
corleyma <mattcorley@gmail.com>
2016-07-24 02:10:59 -0700

    changing more paths to be of the format src.MODULE  (based on Sentry error logs)

--- src/djangoreactredux/urls.py
+++ src/djangoreactredux/urls.py
@@ -5,11 +5,11 @@ from django.views.decorators.cache import cache_page
 from src.base import views as base_views
 
 urlpatterns = [
-    url(r'^api/v1/accounts/', include('accounts.urls', namespace='accounts')),
-    url(r'^api/v1/getdata/', include('base.urls', namespace='base')),
+    url(r'^api/v1/accounts/', include('src.accounts.urls', namespace='accounts')),
+    url(r'^api/v1/getdata/', include('src.base.urls', namespace='base')),
 
     # url(r'^api/v1/getalbums/1', include('albums.urls', namespace='albums')),
-    url(r'^api/v1/getalbums/', include('albums.urls', namespace='albums')),
+    url(r'^api/v1/getalbums/', include('src.albums.urls', namespace='albums')),
 
     # catch all others because of how history is handled by react router - cache this page because it will never change
     url(r'', cache_page(settings.PAGE_CACHE_SECONDS)(base_views.IndexView.as_view()), name='index'),
