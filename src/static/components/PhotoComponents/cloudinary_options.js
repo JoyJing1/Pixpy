454ab3d7e4bd4daf6cf23c5d66cc3f632fee0425
joy <joy.jing1@gmail.com>
2016-07-24 20:39:11 -0700

    Merge master into joy

---
+++ src/static/components/PhotoComponents/cloudinary_options.js
@@ -0,0 +1,9 @@
+const CLOUDINARY_OPTIONS = {
+    cloud_name: 'joyjing1',
+    upload_preset: 'friendeximgs',
+    theme: 'minimal',
+    client_allowed_formats: ['png', 'gif', 'jpeg'],
+    sources: ['local', 'url']
+};
+
+export default CLOUDINARY_OPTIONS;
